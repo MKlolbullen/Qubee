3190aafb973a292e7d57910b61f6ca426b0868cb
MKlolbullen <victor.ahgren@gmail.com>
2025-12-31 16:38:32 +0100

    tetst

---
+++ app/src/main/java/com/qubee/messenger/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.qubee.messenger.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val QubeeDarkBg = Color(0xFF121212)       // Huvudbakgrund (nästan svart)
+val QubeeSurface = Color(0xFF1E1E1E)      // Paneler/TopBar (mörkgrå)
+val QubeeMyBubble = Color(0xFF2C6836)     // Min bubbla (dämpad Signal-grön)
+val QubeeTheirBubble = Color(0xFF2A2A2A)  // Deras bubbla (mörkgrå)
+val QubeeTextPrimary = Color(0xFFEEEEEE)  // Vit text
+val QubeeTextSecondary = Color(0xFFAAAAAA)// Grå text (tidsstämplar)
+val QubeeAccent = Color(0xFF4CAF50)       // Ikoner/Knappar

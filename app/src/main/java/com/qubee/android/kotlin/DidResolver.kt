e83981904e122ca66e35c83267217e5383d57479
0xlolbullen <53312929+MKlolbullen@users.noreply.github.com>
2026-01-04 14:26:32 +0100

    Delete app/src/main/java/com/qubee/android directory

--- app/src/main/java/com/qubee/android/kotlin/DidResolver.kt
+++
@@ -1,7 +0,0 @@
-class DidResolver {
-  suspend fun resolve(did: String): DidDocument {
-    val url = "https://uniresolver.io/1.0/identifiers/$did"
-    val json = httpClient.get<String>(url)
-    return jsonAdapter.fromJson(json)!!
-  }
-}
